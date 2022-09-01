58bbdfc82f1c67e991b9847335637f05c8021c34
ananev.ea <ananev.ea@zvtr.ru>
2022-09-01 10:09:40 +0300

    SBTR-208 перенос core-tools в травелайн

--- pkg/amqp/task.go
+++ pkg/amqp/task.go
@@ -2,14 +2,21 @@ package amqp
 
 type Task interface {
 	IncrAttemptNumber() int64
+	GetFailedCallback() func()
 }
 
 type BaseTask struct {
-	AttemptNumber int64 `json:"attempt_number"`
+	AttemptNumber  int64  `json:"attempt_number"`
+	FailedCallback func() `json:"-"`
 }
 
 // IncrAttemptNumber инкрементирует количество попыток и возвращает новое значение
 func (t *BaseTask) IncrAttemptNumber() int64 {
 	t.AttemptNumber++
 	return t.AttemptNumber
+}
+
+// GetFailedCallback возвращает функцию, которая срабатывает в случае неудачного выполнения задачи после всех попыток
+func (t BaseTask) GetFailedCallback() func() {
+	return t.FailedCallback
 }
