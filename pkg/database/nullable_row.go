63e6383ac997626dcb95a51dc8275ac66fa20510
ananev.ea <ananev.ea@zvtr.ru>
2022-09-11 22:08:24 +0300

    SBTR-467 добавил для join работу с nullable сущностями

---
+++ pkg/database/nullable_row.go
@@ -0,0 +1,148 @@
+package database
+
+import (
+	"reflect"
+	"strings"
+	"time"
+
+	"github.com/guregu/null"
+)
+
+// DestHasNullableRelations проверяем, есть ли сущности join с nullable
+func DestHasNullableRelations(vDest reflect.Value, relations ...string) bool {
+	if len(relations) == 0 {
+		return false
+	}
+
+	for i := 0; i < vDest.NumField(); i++ {
+		field := vDest.Field(i)
+		tag := vDest.Type().Field(i).Tag
+
+		if field.Kind() == reflect.Struct {
+			for _, relation := range relations {
+				if strings.Contains(tag.Get("relation"), relation) && strings.Contains(tag.Get("relation"), "nullable") {
+					return true
+				}
+			}
+		}
+	}
+
+	return false
+}
+
+// TransformDestToNullable переделываем изачальное поле с go типом в nullable тип
+func TransformDestToNullable(field reflect.Value) any {
+	newField := reflect.New(field.Type()).Interface()
+
+	switch field.Kind() {
+	case reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64, reflect.Uint, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int, reflect.Int64:
+		newField = reflect.New(reflect.TypeOf(null.Int{})).Interface()
+	case reflect.Float32, reflect.Float64:
+		newField = reflect.New(reflect.TypeOf(null.Float{})).Interface()
+	case reflect.Bool:
+		newField = reflect.New(reflect.TypeOf(null.Bool{})).Interface()
+	case reflect.String:
+		newField = reflect.New(reflect.TypeOf(null.String{})).Interface()
+	default:
+		if field.Type().Name() == reflect.TypeOf(time.Time{}).Name() {
+			newField = reflect.New(reflect.TypeOf(null.Time{})).Interface()
+		}
+	}
+
+	return newField
+}
+
+// GetNullableRowFromOrigDest получаем слайс интерфейсов с nullable полями в join сущностях
+func GetNullableRowFromOrigDest(vDest reflect.Value, nullable bool, relations ...string) (newItem []any) {
+	for i := 0; i < vDest.NumField(); i++ {
+		field := vDest.Field(i)
+		tag := vDest.Type().Field(i).Tag
+
+		if tag.Get("db") != "" {
+			if !nullable {
+				newItem = append(newItem, reflect.New(field.Type()).Interface())
+			} else {
+				newItem = append(newItem, TransformDestToNullable(field))
+			}
+		} else {
+			if field.Kind() == reflect.Struct && len(relations) > 0 {
+				for _, relation := range relations {
+					if tag.Get("relation") != "" && strings.Contains(tag.Get("relation"), relation) {
+						newItem = append(newItem, GetNullableRowFromOrigDest(field, strings.Contains(tag.Get("relation"), "nullable"))...)
+					}
+				}
+			}
+		}
+	}
+
+	return newItem
+}
+
+// SetNullableDestFields сканируем слайс с nullable интерфейсами
+func SetNullableDestFields(vDest reflect.Value, scanFields []any) []any {
+	for i := 0; i < vDest.Len(); i++ {
+		field := vDest.Index(i)
+
+		if field.Kind() == reflect.Slice {
+			scanFields = SetNullableDestFields(field, scanFields)
+		} else {
+			scanFields = append(scanFields, field.Interface())
+		}
+	}
+
+	return scanFields
+}
+
+// TransformNullableToDest передаем значение из ранее переделанного nullable поля в изначальный тип
+func TransformNullableToDest(fieldOrig reflect.Value, field any) reflect.Value {
+	switch fieldOrig.Kind() {
+	case reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64, reflect.Uint:
+		fieldOrig.SetUint(uint64((*(field.(*null.Int))).Int64))
+	case reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int, reflect.Int64:
+		fieldOrig.SetInt((*(field.(*null.Int))).Int64)
+	case reflect.Float32:
+		fieldOrig.SetFloat((*(field.(*null.Float))).Float64)
+	case reflect.Bool:
+		fieldOrig.SetBool((*(field.(*null.Bool))).Bool)
+	case reflect.String:
+		fieldOrig.SetString((*(field.(*null.String))).String)
+	default:
+		if fieldOrig.Type().Name() == reflect.TypeOf(time.Time{}).Name() {
+			fieldOrig = reflect.ValueOf((*(field.(*null.Time))).Time)
+		}
+	}
+
+	return fieldOrig
+}
+
+// SetDestFromNullable проставляем изначальной сущности поля из nullable слайса
+func SetDestFromNullable(vDestOrig reflect.Value, vDest []any, nullable bool, iDest int, relations ...string) (reflect.Value, int) {
+	for i := 0; i < vDestOrig.NumField(); i++ {
+		fieldOrig := vDestOrig.Field(i)
+		tag := vDestOrig.Type().Field(i).Tag
+
+		if len(vDest) == iDest {
+			continue
+		}
+		field := vDest[iDest]
+
+		if tag.Get("db") != "" {
+			if nullable {
+				fieldOrig.Set(TransformNullableToDest(fieldOrig, field))
+			} else {
+				fieldOrig.Set(reflect.ValueOf(field).Elem())
+			}
+			iDest++
+		} else if fieldOrig.Kind() == reflect.Struct && len(relations) > 0 {
+			for _, relation := range relations {
+				if tag.Get("relation") != "" && strings.Contains(tag.Get("relation"), relation) {
+					setField, newIDest := SetDestFromNullable(fieldOrig, vDest, strings.Contains(tag.Get("relation"), "nullable"), iDest)
+					iDest = newIDest
+					fieldOrig.Set(setField)
+				}
+			}
+		}
+	}
+
+	return vDestOrig, iDest
+}
--- pkg/database/service.go
+++ pkg/database/service.go
@@ -3,6 +3,7 @@ package database
 import (
 	"context"
 	"reflect"
+	"strings"
 
 	"github.com/doug-martin/goqu/v9"
 	"github.com/jackc/pgx/v4"
@@ -11,13 +12,13 @@ import (
 
 type DBService interface {
 	Dialect() goqu.DialectWrapper
-	Exec(ctx context.Context, query string, args []interface{}) error
-	Insert(ctx context.Context, sql string, args []interface{}, dest interface{}) error
-	Count(ctx context.Context, sql string, args []interface{}) (int64, error)
-	SelectOne(ctx context.Context, sql string, args []interface{}, dest interface{}, relations ...string) error
-	Select(ctx context.Context, sql string, args []interface{}, dest interface{}, relations ...string) error
+	Exec(ctx context.Context, query string, args []any) error
+	Insert(ctx context.Context, sql string, args []any, dest any) error
+	Count(ctx context.Context, sql string, args []any) (int64, error)
+	SelectOne(ctx context.Context, sql string, args []any, dest any, relations ...string) error
+	Select(ctx context.Context, sql string, args []any, dest any, relations ...string) error
 	Begin(ctx context.Context) (pgx.Tx, error)
-	InsertMany(ctx context.Context, sql string, args []interface{}, dest interface{}) error
+	InsertMany(ctx context.Context, sql string, args []any, dest any) error
 }
 
 type dbService struct {
@@ -37,21 +38,21 @@ func (s *dbService) Dialect() goqu.DialectWrapper {
 }
 
 // Exec выполняет запрос
-func (s *dbService) Exec(ctx context.Context, query string, args []interface{}) error {
+func (s *dbService) Exec(ctx context.Context, query string, args []any) error {
 	_, err := s.pool.Exec(ctx, query, args...)
 
 	return err
 }
 
 // Insert выполняет insert запрос и возвращает данные в dest
-func (s *dbService) Insert(ctx context.Context, sql string, args []interface{}, dest interface{}) error {
+func (s *dbService) Insert(ctx context.Context, sql string, args []any, dest any) error {
 	err := s.pool.QueryRow(ctx, sql, args...).Scan(dest)
 
 	return err
 }
 
 // InsertMany выполняет insert запрос и возвращает множественные данные в dest
-func (s *dbService) InsertMany(ctx context.Context, sql string, args []interface{}, dest interface{}) error {
+func (s *dbService) InsertMany(ctx context.Context, sql string, args []any, dest any) error {
 	rows, err := s.pool.Query(ctx, sql, args...)
 	if err != nil {
 		return err
@@ -65,7 +66,7 @@ func (s *dbService) InsertMany(ctx context.Context, sql string, args []interface
 }
 
 // Select выполняет SELECT запрос и сохраняет результаты в массив структур dest
-func (s *dbService) Select(ctx context.Context, sql string, args []interface{}, dest interface{}, relations ...string) error {
+func (s *dbService) Select(ctx context.Context, sql string, args []any, dest any, relations ...string) error {
 	rows, err := s.pool.Query(ctx, sql, args...)
 	if err != nil {
 		return err
@@ -79,7 +80,7 @@ func (s *dbService) Select(ctx context.Context, sql string, args []interface{}, 
 }
 
 // SelectOne выполняет SELECT запрос и сохраняет только первый результат в структуру dest
-func (s *dbService) SelectOne(ctx context.Context, sql string, args []interface{}, dest interface{}, relations ...string) error {
+func (s *dbService) SelectOne(ctx context.Context, sql string, args []any, dest any, relations ...string) error {
 	row := s.pool.QueryRow(ctx, sql, args...)
 
 	if err := scanRow(row, dest, relations...); err != nil {
@@ -90,7 +91,7 @@ func (s *dbService) SelectOne(ctx context.Context, sql string, args []interface{
 }
 
 // Count выполняет COUNT запрос
-func (s *dbService) Count(ctx context.Context, sql string, args []interface{}) (int64, error) {
+func (s *dbService) Count(ctx context.Context, sql string, args []any) (int64, error) {
 	row := s.pool.QueryRow(ctx, sql, args...)
 
 	var count int64
@@ -106,19 +107,32 @@ func (s *dbService) Begin(ctx context.Context) (pgx.Tx, error) {
 	return s.pool.Begin(ctx)
 }
 
-func scanRows(rows pgx.Rows, dest interface{}, relations ...string) error {
-
+func scanRows(rows pgx.Rows, dest any, relations ...string) error {
 	vDest := reflect.ValueOf(dest).Elem()
+
+	nullable := DestHasNullableRelations(reflect.New(reflect.TypeOf(dest).Elem().Elem()).Elem(), relations...)
+
+	var nullableRow, scanFields []any
+	if nullable {
+		nullableRow = GetNullableRowFromOrigDest(reflect.New(reflect.TypeOf(dest).Elem().Elem()).Elem(), false, relations...)
+		scanFields = SetNullableDestFields(reflect.ValueOf(nullableRow), []any{})
+		if err := rows.Scan(scanFields...); err != nil {
+			return err
+		}
+	}
 
 	for rows.Next() {
 		// reflect.TypeOf(dest) это указатель
 		// первый Elem() разыименовывает его, второй Elem() получает типо элемента в slice
 		destItem := reflect.New(reflect.TypeOf(dest).Elem().Elem()).Elem()
-		var scanFields []interface{}
-		scanFields = setDestFields(destItem, scanFields, relations...)
 
-		if err := rows.Scan(scanFields...); err != nil {
-			return err
+		if nullable {
+			SetDestFromNullable(destItem, nullableRow, false, 0, relations...)
+		} else {
+			scanFields = setDestFields(destItem, []any{}, relations...)
+			if err := rows.Scan(scanFields...); err != nil {
+				return err
+			}
 		}
 
 		vDest.Set(reflect.Append(vDest, destItem))
@@ -127,20 +141,31 @@ func scanRows(rows pgx.Rows, dest interface{}, relations ...string) error {
 	return rows.Err()
 }
 
-func scanRow(row pgx.Row, dest interface{}, relations ...string) error {
+func scanRow(row pgx.Row, dest any, relations ...string) error {
 	vDest := reflect.ValueOf(dest).Elem()
 
-	var scanFields []interface{}
-	scanFields = setDestFields(vDest, scanFields, relations...)
+	nullable := DestHasNullableRelations(vDest, relations...)
 
-	if err := row.Scan(scanFields...); err != nil {
-		return err
+	if nullable {
+		nullableRow := GetNullableRowFromOrigDest(vDest, false, relations...)
+
+		scanFields := SetNullableDestFields(reflect.ValueOf(nullableRow).Elem(), []any{})
+		if err := row.Scan(scanFields...); err != nil {
+			return err
+		}
+
+		SetDestFromNullable(vDest, nullableRow, false, 0, relations...)
+	} else {
+		scanFields := setDestFields(vDest, []any{}, relations...)
+		if err := row.Scan(scanFields...); err != nil {
+			return err
+		}
 	}
 
 	return nil
 }
 
-func setDestFields(vDest reflect.Value, scanFields []interface{}, relations ...string) []interface{} {
+func setDestFields(vDest reflect.Value, scanFields []any, relations ...string) []any {
 
 	for i := 0; i < vDest.NumField(); i++ {
 		field := vDest.Field(i)
@@ -149,7 +174,7 @@ func setDestFields(vDest reflect.Value, scanFields []interface{}, relations ...s
 
 		if field.Kind() == reflect.Struct && len(relations) > 0 {
 			for _, relation := range relations {
-				if relation == tag.Get("relation") {
+				if strings.Contains(tag.Get("relation"), relation) {
 					scanFields = setDestFields(field, scanFields)
 				}
 			}
