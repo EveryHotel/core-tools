b14d6b0e78b63af77a3a0315f01d637327afddcc
Dmitrii Kolbin <dkolbin@gmail.com>
2026-05-14 19:55:47 +0400

    #HS-832 переделал работу с amqp при publish больше не создается новый publisher, а переиспользуетя существующий, если есть, так же и с его коннектом, если чего-то из этого нет - пересоздает коннект и паблишер

--- pkg/amqp/logger.go
+++ pkg/amqp/logger.go
@@ -6,45 +6,49 @@ import (
 )
 
 type logger struct {
-	level slog.Level
+	level  slog.Level
+	prefix string
 }
 
-func NewLogger(level slog.Level) logger {
-	return logger{level: level}
+func NewLogger(level slog.Level, prefix string) logger {
+	return logger{
+		level:  level,
+		prefix: fmt.Sprintf("amqp: %s:", prefix),
+	}
 }
 
-func (l logger) Fatalf(format string, v ...interface{}) {
+func (l logger) Fatalf(format string, v ...any) {
 	if l.level >= slog.LevelError {
-		slog.Error(fmt.Sprintf("amqp: "+format, v...))
+		slog.Error(fmt.Sprintf(l.prefix+format, v...))
 	}
 }
 
-func (l logger) Errorf(format string, v ...interface{}) {
+func (l logger) Errorf(format string, v ...any) {
 	if l.level <= slog.LevelError {
-		slog.Error(fmt.Sprintf("amqp: "+format, v...))
+		slog.Error(fmt.Sprintf(l.prefix+format, v...))
 	}
 }
 
-func (l logger) Warnf(format string, v ...interface{}) {
+func (l logger) Warnf(format string, v ...any) {
 	if l.level <= slog.LevelWarn {
-		slog.Warn(fmt.Sprintf("amqp: "+format, v...))
+		slog.Warn(fmt.Sprintf(l.prefix+format, v...))
 	}
 }
 
-func (l logger) Infof(format string, v ...interface{}) {
+func (l logger) Infof(format string, v ...any) {
 	if l.level <= slog.LevelInfo {
-		slog.Info(fmt.Sprintf("amqp: "+format, v...))
+		slog.Info(fmt.Sprintf(l.prefix+format, v...))
 	}
 }
 
-func (l logger) Debugf(format string, v ...interface{}) {
+func (l logger) Debugf(format string, v ...any) {
 	if l.level <= slog.LevelDebug {
-		slog.Debug(fmt.Sprintf("amqp: "+format, v...))
+		slog.Debug(fmt.Sprintf(l.prefix+format, v...))
 	}
 }
 
-func (l logger) Tracef(format string, v ...interface{}) {
+func (l logger) Tracef(format string, v ...any) {
 	if l.level <= slog.LevelDebug {
-		slog.Debug(fmt.Sprintf("amqp: "+format, v...))
+		slog.Debug(fmt.Sprintf(l.prefix+format, v...))
 	}
 }
--- pkg/amqp/service.go
+++ pkg/amqp/service.go
@@ -2,9 +2,13 @@ package amqp
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
 	"log/slog"
+	"strings"
+	"sync"
 
+	"github.com/rabbitmq/amqp091-go"
 	"github.com/wagslane/go-rabbitmq"
 )
 
@@ -26,7 +30,10 @@ type amqpService struct {
 	exchangeName      string
 	exchangeType      string
 	consumers         []ConsumerService
+	connMu            sync.Mutex
 	connection        *rabbitmq.Conn
+	pubMu             sync.Mutex
+	publisher         *rabbitmq.Publisher
 	internalConsumers []*rabbitmq.Consumer
 	logLevel          slog.Level
 }
@@ -40,6 +47,85 @@ func NewAmqpService(amqpUrl string, exchangeName, exchangeType string) AmqpServi
 	}
 }
 
+func (s *amqpService) getConnection(logMsg string) (*rabbitmq.Conn, error) {
+	s.connMu.Lock()
+	defer s.connMu.Unlock()
+
+	if s.connection != nil {
+		return s.connection, nil
+	}
+
+	conn, err := rabbitmq.NewConn(
+		s.amqpUrl,
+		rabbitmq.WithConnectionOptionsLogger(NewLogger(s.logLevel, logMsg)),
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	s.connection = conn
+	return s.connection, nil
+}
+
+func (s *amqpService) resetConnection() {
+	s.connMu.Lock()
+	defer s.connMu.Unlock()
+
+	s.pubMu.Lock()
+	if s.publisher != nil {
+		s.publisher.Close()
+		s.publisher = nil
+	}
+	s.pubMu.Unlock()
+
+	if s.connection != nil {
+		_ = s.connection.Close()
+		s.connection = nil
+	}
+}
+
+func (s *amqpService) getPublisher(logMsg string) (*rabbitmq.Publisher, error) {
+	s.pubMu.Lock()
+	defer s.pubMu.Unlock()
+
+	if s.publisher != nil {
+		return s.publisher, nil
+	}
+
+	conn, err := s.getConnection(logMsg)
+	if err != nil {
+		return nil, err
+	}
+
+	publisher, err := rabbitmq.NewPublisher(conn,
+		rabbitmq.WithPublisherOptionsExchangeName(s.exchangeName),
+		rabbitmq.WithPublisherOptionsExchangeDeclare,
+		rabbitmq.WithPublisherOptionsExchangeKind(s.exchangeType),
+		rabbitmq.WithPublisherOptionsExchangeDurable,
+		rabbitmq.WithPublisherOptionsLogger(
+			NewLogger(
+				s.logLevel,
+				fmt.Sprintf("publisher exchange: %s", s.exchangeName),
+			)),
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	s.publisher = publisher
+	return s.publisher, nil
+}
+
+func isConnNotOpenErr(err error) bool {
+	if err == nil {
+		return false
+	}
+	msg := err.Error()
+	return errors.Is(err, amqp091.ErrClosed) ||
+		strings.Contains(msg, "Exception (504)") ||
+		strings.Contains(msg, "reconnecting to amqp server")
+}
+
 func (s *amqpService) SetLogLevel(level slog.Level) {
 	s.logLevel = level
 }
@@ -51,44 +137,39 @@ func (s *amqpService) AddConsumer(consumer ConsumerService) {
 
 // Publish публикует новую задачу в пулл по заданным routingKeys
 func (s *amqpService) Publish(task Task, routingKeys []string) error {
-	conn, err := rabbitmq.NewConn(
-		s.amqpUrl,
-		rabbitmq.WithConnectionOptionsLogging,
-	)
-	if err != nil {
-		return err
+	publishOnce := func() error {
+		publisher, err := s.getPublisher(
+			fmt.Sprintf(
+				"connection for publish exchange: %s routing: %s",
+				s.exchangeName,
+				strings.Join(routingKeys, ", "),
+			),
+		)
+		if err != nil {
+			return err
+		}
+
+		message, err := json.Marshal(task)
+		if err != nil {
+			return err
+		}
+
+		return publisher.Publish(
+			message,
+			routingKeys,
+			rabbitmq.WithPublishOptionsContentType("application/json"),
+			rabbitmq.WithPublishOptionsMandatory,
+			rabbitmq.WithPublishOptionsPersistentDelivery,
+			rabbitmq.WithPublishOptionsExchange(s.exchangeName),
+		)
 	}
 
-	defer conn.Close()
-
-	publisher, err := rabbitmq.NewPublisher(conn,
-		rabbitmq.WithPublisherOptionsLogging,
-		rabbitmq.WithPublisherOptionsExchangeName(s.exchangeName),
-		rabbitmq.WithPublisherOptionsExchangeDeclare,
-		rabbitmq.WithPublisherOptionsExchangePassive,
-		rabbitmq.WithPublisherOptionsExchangeDurable,
-		rabbitmq.WithPublisherOptionsLogger(NewLogger(s.logLevel)),
-	)
-	if err != nil {
-		return err
-	}
-	defer publisher.Close()
-
-	task.IncrAttemptNumber()
-	message, err := json.Marshal(task)
-	if err != nil {
-		return err
-	}
-
-	err = publisher.Publish(
-		message,
-		routingKeys,
-		rabbitmq.WithPublishOptionsContentType("application/json"),
-		rabbitmq.WithPublishOptionsMandatory,
-		rabbitmq.WithPublishOptionsPersistentDelivery,
-		rabbitmq.WithPublishOptionsExchange(s.exchangeName),
-	)
-	if err != nil {
+	// 1-я попытка
+	if err := publishOnce(); err != nil {
+		if isConnNotOpenErr(err) {
+			s.resetConnection()
+			return publishOnce()
+		}
 		return err
 	}
 
@@ -108,15 +189,16 @@ func (s *amqpService) Serve() error {
 
 // runConsumer запускает обработчика задач в работу
 func (s *amqpService) runConsumer(consumerService ConsumerService) error {
-	var err error
-	if s.connection == nil {
-		s.connection, err = rabbitmq.NewConn(
-			s.amqpUrl,
-			rabbitmq.WithConnectionOptionsLogging,
-		)
-		if err != nil {
-			return err
-		}
+	conn, err := s.getConnection(
+		fmt.Sprintf(
+			"connection for consumer exchange: %s queue: %s routing: %s",
+			s.exchangeName,
+			consumerService.GetQueueName(),
+			strings.Join(consumerService.GetRoutingKeys(), ", "),
+		),
+	)
+	if err != nil {
+		return err
 	}
 
 	handler := func(message rabbitmq.Delivery) rabbitmq.Action {
@@ -131,18 +213,22 @@ func (s *amqpService) runConsumer(consumerService ConsumerService) error {
 		rabbitmq.WithConsumerOptionsQueueDurable,
 		rabbitmq.WithConsumerOptionsExchangeDurable,
 		rabbitmq.WithConsumerOptionsConcurrency(consumerService.GetConcurrency()),
-		rabbitmq.WithConsumerOptionsLogger(NewLogger(s.logLevel)),
+		rabbitmq.WithConsumerOptionsLogger(
+			NewLogger(
+				s.logLevel,
+				fmt.Sprintf("consumer exchange: %s queue: %s routing: %s", s.exchangeName, consumerService.GetQueueName(), strings.Join(consumerService.GetRoutingKeys(), ", ")),
+			)),
 	}
 
 	for _, routing := range consumerService.GetRoutingKeys() {
 		opts = append(opts, rabbitmq.WithConsumerOptionsRoutingKey(routing))
 	}
 
-	consumer, err := rabbitmq.NewConsumer(s.connection,
+	consumer, err := rabbitmq.NewConsumer(
+		conn,
 		consumerService.GetQueueName(),
 		opts...,
 	)
-
 	if err != nil {
 		return err
 	}
@@ -184,8 +270,19 @@ func (s *amqpService) Close() error {
 		consumer.Close()
 	}
 
+	s.pubMu.Lock()
+	if s.publisher != nil {
+		s.publisher.Close()
+		s.publisher = nil
+	}
+	s.pubMu.Unlock()
+
+	s.connMu.Lock()
+	defer s.connMu.Unlock()
+
 	if s.connection != nil {
 		err := s.connection.Close()
+		s.connection = nil
 		if err != nil {
 			return err
 		}
